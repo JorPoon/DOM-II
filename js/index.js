eae6b99732d0d89b43f734e1cf4535349f9eeddd
Jor_Poon <jormpoon@gmail.com>
2019-01-29 16:10:25 -0500

    checked if only first sign up button has event listener

--- js/index.js
+++ js/index.js
@@ -4,3 +4,9 @@
 const signBtn = document.querySelectorAll(".btn");
 
 console.log(signBtn.length);
+
+console.log(signBtn[0]);
+
+signBtn[0].addEventListener("click", event => {
+    console.log("hello");
+})
