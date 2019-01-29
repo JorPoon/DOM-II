01f799ea3c1f6810ef4dba71b31fd7acb9ecf890
Jor_Poon <jormpoon@gmail.com>
2019-01-29 16:20:49 -0500

    added click count to first sign up button and created two new event listeners for buttons 2 and 3

--- js/index.js
+++ js/index.js
@@ -4,9 +4,16 @@
 const signBtn = document.querySelectorAll(".btn");
 
 console.log(signBtn.length);
-
 console.log(signBtn[0]);
 
 signBtn[0].addEventListener("click", event => {
+    signBtn[0].innerHTML = `Click count ${event.detail}`;
+})
+
+signBtn[1].addEventListener("click", event => {
+    console.log("hello");
+})
+
+signBtn[2].addEventListener("click", event => {
     console.log("hello");
 })
